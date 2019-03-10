ac229920f3d25b8eddcfb264a5c4b6bb7e186c7d
Clemon-R <raphael.goulmot@epitech.eu>
2019-03-10 17:48:47 +0100

    Service Youtube

---
+++ client_web/src/app/pages/youtube/callback/youtube-callback.component.ts
@@ -0,0 +1,65 @@
+import {Component, OnInit} from '@angular/core';
+import {YoutubeService} from '../youtube.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {LayoutService} from '../../../layout/layout.service';
+import { Account } from '../../../models/account';
+import {ResultViewModel} from '../../../viewModels/ResultViewModel';
+
+@Component({
+  selector: 'app-spotify-callback',
+  templateUrl: './youtube-callback.component.html',
+  styleUrls: ['./youtube-callback.component.css']
+})
+export class YoutubeCallbackComponent implements OnInit {
+  code: string;
+  account: Account;
+  error: boolean;
+  message: string;
+  constructor(
+    private layoutService: LayoutService,
+    private youtubeService: YoutubeService,
+    private route: ActivatedRoute,
+    private router: Router) {
+    this.error = false;
+    this.message = 'Veuillez attendre un instant s\'il vous plez';
+
+    this.account = JSON.parse(localStorage.getItem('account')) as Account;
+    if (this.account == null) {
+      this.router.navigate(['/disconnected']);
+      return;
+    }
+  }
+
+  ngOnInit() {
+    this.account = JSON.parse(localStorage.getItem('account')) as Account;
+    if (this.account == null) {
+      this.router.navigate(['/disconnected']);
+      return;
+    }
+    console.log('Trying to find the code...');
+    this.route.queryParams.subscribe(params => {
+      if (!params.hasOwnProperty('code') || this.account == null) {
+        return;
+      }
+      this.code = params.code;
+      console.log('Code: ' + this.code);
+      if (this.code) {
+        this.youtubeService.getToken(this.account, this.code).then(
+          (result: ResultViewModel) => {
+            if (result.success) {
+              this.error = false;
+              this.message = 'Votre compte à bien été connecté';
+            } else {
+              this.error = true;
+              this.message = result.error;
+            }
+          },
+          (error) => {
+            this.error = true;
+            this.message = 'Une erreur est survenue';
+          }
+        );
+      }
+    });
+  }
+}
