f29a2d2326c202d672315c7ead78d53423e55e16
Clemon-R <raphael.goulmot@epitech.eu>
2019-03-11 23:13:16 +0100

    Updating Apis

---
+++ server/Wrappers/Youtube/Models/YoutubeRefreshTokenModel.cs
@@ -0,0 +1,15 @@
+﻿using Area.Wrappers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Area.Wrappers.Youtube.Models
+{
+    public class YoutubeRefreshTokenModel : RequestSuccessModel
+    {
+        public string Access_Token { get; set; }
+        public int Expires_In { get; set; }
+        public string Token_Type { get; set; }
+    }
+}
